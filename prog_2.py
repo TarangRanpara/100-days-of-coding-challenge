8e388b20807bb43d788bbb9786dd498b5d209621
Tarang Ranpara <tarangranpara1998@gmail.com>
2018-08-31 22:51:31 +0530

    Update prog_2.py

--- prog_2.py
+++ prog_2.py
@@ -1,5 +1,11 @@
 from random import randint
 
+'''
+You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age.
+When she blows out the candles, she’ll only be able to blow out the tallest ones. 
+Your task is to find out how many candles she can successfully blow out.
+'''
+
 def get_counts(age):
     l = [randint(1,4) for i in range(age)]
     return l.count(max(l))
