5a7954b6408b0ef07701356eaefc02b0727056aa
Tarang Ranpara <tarangranpara1998@gmail.com>
2018-10-20 11:38:45 -0700

    Add files via upload

---
+++ again/day_7.py
@@ -0,0 +1,12 @@
+def main():
+    s,n= input('enter string:'),int(input('enter n:'))
+
+    if n < len(s):
+        print(s.count('a'))
+
+    else:
+        repeat = int(n/len(s))
+        print(repeat*(s.count('a')) + s[:(n % len(s))].count('a'))
+
+if __name__ == '__main__':
+    main()
